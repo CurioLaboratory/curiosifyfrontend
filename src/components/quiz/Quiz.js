d125e7dca5209a7f3db5fc50943d215bc3ab57c3
Gaurav Shresth <gaurav.shresth2000@gmail.com>
2024-07-26 11:15:12 +0530

    css changes

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Quiz.scss';
 import axiosInstance from "../../axiosInstance";
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
 
 const Quiz = ({ onCreateQuiz }) => {
   const [quizzes, setQuizzes] = useState([]);
@@ -20,41 +21,25 @@ const Quiz = ({ onCreateQuiz }) => {
     </div>
   );
   useEffect(() => {
-    // Simulating a fetch request to the backend with dummy data
-    // setTimeout(() => {
-    //   const dummyData = [
-    //     { id: 1, name: 'Evolution', numOfQuestions: 2, class: 12, createdOn: '01-03-2024' },
-    //     { id: 2, name: 'Laws of motion', numOfQuestions: 1, class: 11, createdOn: '01-03-2024' },
-    //   ];
-    //   const data = localStorage.getItem('quizzes');
-    //   // console.log(data)
-    //   setQuizzes(data);
 
-    //   setLoading(false);
-    // }, 10); // Simulating network delay
     const getAllQuiz = async () => {
       const response = await axiosInstance.get("/quiz/getallquiz");
       setQuizzes(response.data);
-      // console.log(response.data);
       setLoading(false);
     };
 
     getAllQuiz();
   }, [refreshAllQuiz]);
 
-  const handleCheckboxChange = (id) => {
-    setSelectedQuizzes((prevSelectedQuizzes) =>
-      prevSelectedQuizzes.includes(id)
-        ? prevSelectedQuizzes.filter((quizId) => quizId !== id)
-        : [...prevSelectedQuizzes, id]
-    );
-  };
+  // const handleCheckboxChange = (id) => {
+  //   setSelectedQuizzes((prevSelectedQuizzes) =>
+  //     prevSelectedQuizzes.includes(id)
+  //       ? prevSelectedQuizzes.filter((quizId) => quizId !== id)
+  //       : [...prevSelectedQuizzes, id]
+  //   );
+  // };
 
   const handleDelete = async (id) => {
-    // setQuizzes((prevQuizzes) =>
-    //   prevQuizzes.filter((quiz) => !selectedQuizzes.includes(quiz.id))
-    // );
-    // setSelectedQuizzes([]);
     const response = await axiosInstance.delete(`/quiz/deletequiz/${id}`);
 
     if (response.status === 200) {
@@ -69,50 +54,64 @@ const Quiz = ({ onCreateQuiz }) => {
     setSelectedQuiz(quiz);
   };
 
-  // const filteredQuizzes = quizzes
-  //   .filter((quiz) =>
-  //     quiz.name.toLowerCase().includes(searchTerm.toLowerCase())
-  //   )
-  //   .filter((quiz) =>
-  //     selectedClass ? quiz.class === parseInt(selectedClass) : true
-  //   )
-  //   .sort((a, b) => {
-  //     if (sortOption === 'date') {
-  //       return new Date(a.createdOn) - new Date(b.createdOn);
-  //     } else if (sortOption === 'questions') {
-  //       return b.numOfQuestions - a.numOfQuestions;
-  //     }
-  //     return 0;
-  //   });
-  // console.log("Quiz ==>", quizzes);
   if (loading) {
     return <p>Loading...</p>;
   }
+  const getFilteredAndSortedQuizzes = () => {
+    let filteredQuizzes = quizzes;
 
-  // if (selectedQuiz) {
-  //   return (
-  //     <div className="quiz-detail">
-  //       <button className="back-button" onClick={() => setSelectedQuiz(null)}>Back</button>
-  //       <h1>{selectedQuiz.name}</h1>
-  //       <h2>Published for Class {selectedQuiz.class}</h2>
-  //       <div>
-  //         <h3>1. Which of the following is an example of evolution?</h3>
-  //         <p>A deer growing antlers as it matures</p>
-  //         <p>A bird learning to sing a new song</p>
-  //         <p>A fish changing colors to blend in with its surroundings</p>
-  //         <p>A snake shedding its skin</p>
-  //       </div>
-  //       <div>
-  //         <h3>2. What is the term for process which species evolve similar traits due to shared environmental pressures?</h3>
-  //         <p>Convergent evolution</p>
-  //         <p>Divergent evolution</p>
-  //         <p>Co-evolution</p>
-  //         <p>Adaptive radiation</p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+    if (searchTerm) {
+      filteredQuizzes = filteredQuizzes.filter(quiz =>
+        quiz.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
 
+    if (selectedClass) {
+      filteredQuizzes = filteredQuizzes.filter(quiz =>
+        quiz.classLevel.toString() === selectedClass
+      );
+    }
+
+    if (sortOption) {
+      filteredQuizzes.sort((a, b) => {
+        if (sortOption === 'date') {
+          return new Date(b.date) - new Date(a.date);
+        } else if (sortOption === 'questions') {
+          return b.totalQuestions - a.totalQuestions;
+        }
+        return 0;
+      });
+    }
+
+    return filteredQuizzes;
+  };
+
+  if (selectedQuiz) {
+    return (
+      <div className="selectedQuiz" >
+        <button className="back-button" onClick={() => setSelectedQuiz(null)}>← Back</button>
+        <p className="selectedQuizTitle">{selectedQuiz.title}</p>
+        <p >Published for <span className="selectedQuizClass">Class {selectedQuiz.classLevel}</span></p>
+
+        <Card className="text-center cardBody">
+          <Card.Body>
+              {selectedQuiz.questions && selectedQuiz.questions.map((question, index) => (
+                <div className='cardQues' key={index}>
+                  <Card.Text >
+                  <h3>Q{index + 1}. {question.question}</h3>
+                  {question.options.map((option, optionIndex) => (
+                    <p key={optionIndex}>
+                      {String.fromCharCode(65 + optionIndex)}. {option}
+                    </p>
+                  ))}
+                </Card.Text>
+                </div>
+              ))}
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
   return (
     <div className="quiz-page">
       {quizzes.length === 0 ? (
@@ -122,6 +121,37 @@ const Quiz = ({ onCreateQuiz }) => {
           <div className="quizzes-header">
             <h1>Quiz</h1>
             <button className="create-quiz-button" onClick={onCreateQuiz}>+ Create new quiz</button>
+          </div>
+          <div className="quizzes-controls">
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <div className="filters">
+              <select
+                className="filter-by-class"
+                value={selectedClass}
+                onChange={(e) => setSelectedClass(e.target.value)}
+              >
+                <option value="">Filter by Class</option>
+                <option value="9">Class 9</option>
+                <option value="10">Class 10</option>
+                <option value="11">Class 11</option>
+                <option value="12">Class 12</option>
+              </select>
+              <select
+                className="sort-by"
+                value={sortOption}
+                onChange={(e) => setSortOption(e.target.value)}
+              >
+                <option value="">Sort by</option>
+                <option value="date">Date</option>
+                <option value="questions">Number of Questions</option>
+              </select>
+            </div>
           </div>
           <Table striped bordered hover>
             <thead>
@@ -134,37 +164,21 @@ const Quiz = ({ onCreateQuiz }) => {
               </tr>
             </thead>
             <tbody>
-              {quizzes.map((quiz, quizIndex) => (
-                <tr key={quizIndex}>
+              {getFilteredAndSortedQuizzes().map((quiz, quizIndex) => (
+                <tr key={quizIndex} onClick={() => handleQuizClick(quiz)} style={{ cursor: 'pointer' }}>
                   <td>{quiz.title}</td>
                   <td>{quiz.totalQuestions}</td>
                   <td>{quiz.classLevel}</td>
                   <td>{quiz.date}</td>
-                  <td>
-                    <img className="event-delete-button" style={{ cursor: "pointer" }} onClick={() => handleDelete(quiz._id)} src="/icons/dustbin.png" alt="Quiz" />
+                  <td onClick={(e) => e.stopPropagation()}>
+                    <img
+                      className="event-delete-button"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => handleDelete(quiz._id)}
+                      src="/icons/dustbin.png"
+                      alt="Quiz"
+                    />
                   </td>
-                  {/* <h2>{quiz.title}</h2>
-                    <p>Language: {quiz.language}</p>
-                    <p>Class: {quiz.class}</p>
-                    <p>Date Published: {quiz.date}</p>
-                    <p>Total Questions: {quiz.totalQuestions}</p> */}
-                  {/* <div className="questions-container">
-                      {quiz.questions.map((item, questionIndex) => (
-                        <div key={questionIndex} className="quiz-question">
-                          <h3>Question {questionIndex + 1}</h3>
-                          <p>{item.question}</p>
-                          <ul>
-                            {item.options.map((option, optionIndex) => (
-                              <li key={optionIndex} style={{ color: option === item.answer ? 'green' : 'black' }}>
-                                {String.fromCharCode(65 + optionIndex)}: {option}
-                              </li>
-                            ))}
-                          </ul>
-                          <p>Correct Answer: {item.answer}</p>
-                        </div>
-                      ))}
-                    </div> */}
-
                 </tr>
               ))}
             </tbody>
