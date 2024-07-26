1cadfc8ca1cc2558c7aff3376d4e7bf2e6d065d7
Gaurav Shresth <gaurav.shresth2000@gmail.com>
2024-07-26 18:30:42 +0530

    add env

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+const baseURL = process.env.REACT_APP_BASE_URL ;
+// const baseURL ="http://localhost:5000/api";
 
 const instance = axios.create({
     baseURL,
